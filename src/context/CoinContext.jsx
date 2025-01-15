8f4893362a69330f9d86a3ba759ae5eec70262b6
Joaquin <Mintinvestments95@gmail.com>
2025-01-15 17:10:39 -0600

    Started the API process

---
+++ src/context/CoinContext.jsx
@@ -0,0 +1,17 @@
+import { createContext, useState } from "react";
+
+export const CoinContext = createContext();
+
+const [allCoin, setAllCoin] = useState([])
+
+const CoinContextProvider = (props) => {
+  const contextValue = {};
+
+  return (
+    <CoinContext.Provider value={contextValue}>
+      {props.children}
+    </CoinContext.Provider>
+  );
+};
+
+export default CoinContextProvider;

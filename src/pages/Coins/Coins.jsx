a9166f1a370d18f32632b912a37f823d383b4c50
Joaquin Morales <mintinvestments95@gmail.com>
2025-02-03 17:21:38 -0600

    changed title

--- src/pages/Coins/Coins.jsx
+++ src/pages/Coins/Coins.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Coins.css'
+import { useParams } from 'react-router-dom'
 
 const Coins = () => {
+
+  const { coinId } = useParams()
+
   return (
     <div>
-      <h2>Coins</h2>
+      <h2>Coins : {coinId}</h2>
     </div>
   )
 }

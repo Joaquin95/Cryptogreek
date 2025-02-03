fb1ef91ac99a2600cf169fecd673a35ffa7bb597
Joaquin Morales <mintinvestments95@gmail.com>
2025-02-03 17:46:59 -0600

    Styling on Coin page

--- src/pages/Coins/Coins.jsx
+++ src/pages/Coins/Coins.jsx
@@ -1,16 +1,38 @@
-import React from 'react'
-import './Coins.css'
-import { useParams } from 'react-router-dom'
+import React, { useContext, useEffect, useState } from "react";
+import "./Coins.css";
+import { useParams } from "react-router-dom";
+import { CoinContext } from "../../context/CoinContext";
 
 const Coins = () => {
+  const { coinId } = useParams();
+  const [coinData, setCoinData] = useState();
+  const {currency} = useContext(CoinContext);
 
-  const { coinId } = useParams()
+  const fetchCoinData = async () => {
+    const options = {
+      method: "GET",
+      headers: { accept: "application/json" },
+    };
 
+    fetch(`https://pro-api.coingecko.com/api/v3/coins/${coinId}`, options)
+      .then((res) => res.json())
+      .then((res) => setCoinData(res))
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    fetchCoinData();
+  }, [currency]);
+
+  if (!coinData) return <div>Loading...</div>;
   return (
-    <div>
-      <h2>Coins : {coinId}</h2>
+    <div className="coin">
+      <div className="coin-name">
+        <img src={coinData.image.large} alt=""/>
+        <p><b>{coinData.name} ({coinData.symbol.toUpperCase()})</b></p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Coins
+export default Coins;

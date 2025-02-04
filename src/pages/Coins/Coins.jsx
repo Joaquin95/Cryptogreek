2006a2f596834655fbaafed35981475e45ac802a
Joaquin <Mintinvestments95@gmail.com>
2025-02-04 17:38:18 -0600

    added historical data api

--- src/pages/Coins/Coins.jsx
+++ src/pages/Coins/Coins.jsx
@@ -6,6 +6,7 @@ import { CoinContext } from "../../context/CoinContext";
 const Coins = () => {
   const { coinId } = useParams();
   const [coinData, setCoinData] = useState(null);
+  const [historicData, sethistoricData] = useState(null);
   const { currency } = useContext(CoinContext);
 
   const fetchCoinData = async () => {
@@ -20,6 +21,24 @@ const Coins = () => {
     fetch(`https://api.coingecko.com/api/v3/coins/${coinId}`, options)
       .then((res) => res.json())
       .then((res) => setCoinData(res))
+      .catch((err) => console.error(err));
+  };
+
+  const fetchHistoricData = async () => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "x-cg-demo-api-key": "CG-fBbRJNeCebyS8EKjeex4nL99",
+      },
+    };
+
+    fetch(
+      "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=30",
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => sethistoricData(res))
       .catch((err) => console.error(err));
   };
 
